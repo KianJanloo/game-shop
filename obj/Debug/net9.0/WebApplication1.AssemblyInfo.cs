02e60b19886eb77eb04b1ce5cb71815e6b603994
Kian Janloo <ddrdslayer@gmail.com>
2025-11-01 17:32:14 +0330

    Database

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ef3c1e8e9e67bb12103280498ffc1e00a39aad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0245fe0cd078dd161e0506bd31853f198465aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
